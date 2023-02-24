facfb1db97f85f2aa05abc2db65798098f299731
serpentaire <glemoine@hotmail.fr>
2023-02-24 13:45:17 +0100

    solution Angular challenge 11

---
+++ src/app/models/cocktail.model.ts
@@ -0,0 +1,15 @@
+export class Cocktail {
+  name: string;
+  price: number;
+  img: string;
+
+  constructor(
+    name: string,
+    price: number,
+    img: string,
+  ) {
+    this.name = name;
+    this.price = price;
+    this.img = img;
+  }
+}
